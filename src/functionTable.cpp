9d8831b37335f503f33f3b48d582ff980f6e2e00
DengY11 <dengyi123@email.swu.edu.cn>
2024-10-12 00:06:53 +0800

    add some function

--- src/functionTable.cpp
+++ src/functionTable.cpp
@@ -1,5 +1,6 @@
 #include "../include/functionTable.hpp"
 #include <cmath>
+#include <cstddef>
 #include <stdexcept>
 #include <unordered_map>
 namespace myexpr {
@@ -9,17 +10,74 @@ FunctionTable::FunctionTable() {
                      std::function<double(const std::vector<double> &)>>
       initial_funcs{
           {"cos",
-           [](std::vector<double> arg) -> double { return std::cos(arg[0]); }},
+           [](std::vector<double> arg) -> double {
+             if (arg.size() != 1) {
+               throw std::runtime_error(
+                   "function cos needs only one argument!");
+             }
+             return std::cos(arg[0]);
+           }},
           {"sin",
-           [](std::vector<double> arg) -> double { return std::sin(arg[0]); }},
+           [](std::vector<double> arg) -> double {
+             if (arg.size() != 1) {
+               throw std::runtime_error(
+                   "function sin needs only one argument!");
+             }
+             return std::sin(arg[0]);
+           }},
           {"tan",
-           [](std::vector<double> arg) -> double { return std::tan(arg[0]); }},
+           [](std::vector<double> arg) -> double {
+             if (arg.size() != 1) {
+               throw std::runtime_error(
+                   "function tan needs only one argument!");
+             }
+             return std::tan(arg[0]);
+           }},
           {"log",
-           [](std::vector<double> arg) -> double { return std::log(arg[0]); }},
+           [](std::vector<double> arg) -> double {
+             if (arg.size() != 1) {
+               throw std::runtime_error(
+                   "function arg needs only one argument!");
+             }
+             return std::log(arg[0]);
+           }},
           {"sqrt",
-           [](std::vector<double> arg) -> double { return std::sqrt(arg[0]); }},
+           [](std::vector<double> arg) -> double {
+             if (arg.size() != 1) {
+               throw std::runtime_error(
+                   "function sqrt needs only one argument!");
+             }
+             return std::sqrt(arg[0]);
+           }},
           {"abs",
-           [](std::vector<double> arg) -> double { return std::abs(arg[0]); }}
+           [](std::vector<double> arg) -> double {
+             if (arg.size() != 1) {
+               throw std::runtime_error(
+                   "function abs needs only one argument!");
+             }
+             return std::abs(arg[0]);
+           }},
+          {"pow",
+           [](std::vector<double> args) -> double {
+             if (args.size() != 2) {
+               throw std::runtime_error("function pow needs two arguments!");
+             }
+             return std::pow(args[0], args[1]);
+           }},
+          {"averge",
+           [](std::vector<double> args) -> double {
+             if (args.empty()) {
+               throw std::runtime_error(
+                   "function averge needs at least one argument!");
+             }
+             double ret = 0.0;
+             size_t cnt = 0;
+             for (const auto &number : args) {
+               ++cnt;
+               ret += (static_cast<double>(number) - ret) / cnt;
+             }
+             return ret;
+           }}
 
       };
   functions_ = std::move(initial_funcs);
